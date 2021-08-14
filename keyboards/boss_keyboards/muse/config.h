873e4eb015fc30c10bdbb70fd7949501e11f0527
donicrosby <donicrosby1995@gmail.com>
2021-08-14 00:08:45 -0400

    Fix registry

--- keyboards/boss_keyboards/muse/config.h
+++ keyboards/boss_keyboards/muse/config.h
@@ -103,10 +103,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DISABLE_WHEN_USB_SUSPENDED false // turn off effects when suspended
 #define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
 #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CUSTOM_row_one_only // Sets the default mode, if none has been set
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR // Sets the default mode, if none has been set
 #define RGB_MATRIX_STARTUP_HUE 0 // Sets the default hue value, if none has been set
 #define RGB_MATRIX_STARTUP_SAT 0 // Sets the default saturation value, if none has been set
-#define RGB_MATRIX_STARTUP_VAL 128 // Sets the default brightness value, if none has been set
+#define RGB_MATRIX_STARTUP_VAL 255 // Sets the default brightness value, if none has been set
 #define RGB_MATRIX_STARTUP_SPD 127
 //#define RGB_DI_PIN B7
 //#ifdef RGB_DI_PIN
@@ -163,10 +163,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define I2C1_SDA_PAL_MODE 4
 
 #define I2C1_TIMINGR_PRESC  5U
-#define I2C1_TIMINGR_SCLDEL 3U
-#define I2C1_TIMINGR_SDADEL 3U
-#define I2C1_TIMINGR_SCLH   3U
-#define I2C1_TIMINGR_SCLL	9U
+#define I2C1_TIMINGR_SCLL	3U
+#define I2C1_TIMINGR_SCLH   1U
+#define I2C1_TIMINGR_SDADEL 0U
+#define I2C1_TIMINGR_SCLDEL 1U
+
+
 
 /* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
  * This is useful for the Windows task manager shortcut (ctrl+shift+esc).
