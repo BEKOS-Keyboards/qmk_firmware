ee6b8383b474e3b23876e7a5e80caf366c51d1ed
donicrosby <donicrosby1995@gmail.com>
2021-09-05 21:45:15 -0400

    Updated RGB matrix to have all LEDs in the config

--- keyboards/boss_keyboards/muse/keymaps/default/keymap.c
+++ keyboards/boss_keyboards/muse/keymaps/default/keymap.c
@@ -34,6 +34,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
+	// clang-format off
     [_BASE] = LAYOUT(
   		KC_ESC,       KC_F1, KC_F2, KC_F3, KC_F4,    KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9, KC_F10, KC_F11, KC_F12,      KC_PSCR, KC_SLCK, KC_PAUS,      KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, \
   		KC_GRV, KC_1, KC_2,  KC_3,  KC_4,  KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,      KC_BSPC,          KC_INS,  KC_HOME, KC_PGUP,      KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
@@ -46,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   		KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
   		KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
   		KC_TRNS,  RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-  		KC_TRNS,    RGB_RMOD, BKB_RGB_OFF, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,                                           KC_TRNS, KC_TRNS, KC_TRNS,          \
+  		KC_TRNS,    RGB_RMOD, RGB_TOG, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,                                           KC_TRNS, KC_TRNS, KC_TRNS,          \
   		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                          KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
   		KC_TRNS, KC_TRNS,    KC_TRNS,                KC_TRNS,                   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,          KC_TRNS           \
 	),
@@ -66,6 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                          KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
 		KC_TRNS, KC_TRNS,    KC_TRNS,                KC_TRNS,                   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,          KC_TRNS           \
 	)
+	// clang-format on
 };
 
 enum combo_events {
--- keyboards/boss_keyboards/muse/muse_indicator_control.h
+++ keyboards/boss_keyboards/muse/muse_indicator_control.h
@@ -1,13 +1,13 @@
 #pragma once
 
 
-#define CAPS_LOCK_KEY_LED	61 //Specific LED in Matrix (Row 4, 0)
+#define CAPS_LOCK_KEY_LED	61 //Specific LED in Matrix (Row 4, Index 0)
 #define CAPS_LOCK_LED 		DRIVER_LED_TOTAL-5
 
-#define NUM_LOCK_KEY_LED 	36 //Specific LED in Matrix (Row 2, 17)
+#define NUM_LOCK_KEY_LED 	36 //Specific LED in Matrix (Row 2, Index 17)
 #define NUM_LOCK_LED  		DRIVER_LED_TOTAL-4
 
-#define SCROLL_LOCK_KEY_LED 14 //Specific LED in Matrix (Row 1, 14)
+#define SCROLL_LOCK_KEY_LED 14 //Specific LED in Matrix (Row 1, Index 14)
 #define SCROLL_LOCK_LED 	DRIVER_LED_TOTAL-3
 
 
