733475a4b87c7a1169ebebaa534a9e0152dcebb2
donicrosby <donicrosby1995@gmail.com>
2021-09-09 11:10:10 -0400

    Updated config to be more standardized

--- keyboards/boss_keyboards/muse/config.h
+++ keyboards/boss_keyboards/muse/config.h
@@ -113,10 +113,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DISABLE_WHEN_USB_SUSPENDED false // turn off effects when suspended
 #define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
 #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_BAND_PINWHEEL_VAL // Sets the default mode, if none has been set
-#define RGB_MATRIX_STARTUP_HUE 151 // Sets the default hue value, if none has been set
-#define RGB_MATRIX_STARTUP_SAT 227 // Sets the default saturation value, if none has been set
-#define RGB_MATRIX_STARTUP_VAL 189 // Sets the default brightness value, if none has been set
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT // Sets the default mode, if none has been set
+#define RGB_MATRIX_STARTUP_HUE 0 // Sets the default hue value, if none has been set
+#define RGB_MATRIX_STARTUP_SAT 255 // Sets the default saturation value, if none has been set
+#define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS  // Sets the default brightness value, if none has been set
 #define RGB_MATRIX_STARTUP_SPD 127
 //#define RGB_DI_PIN B7
 //#ifdef RGB_DI_PIN
